176a191ec2ecb5cfb678fce3891f65c9b78e6fd7
luciano <lucianogastonbarros@gmail.com>
2022-07-31 22:44:58 -0300

    commit inicial

---
+++ tablas de multiplicar.php
@@ -0,0 +1,10 @@
+<?php
+$multiplicador=20;
+$multiplicado=0;
+for($multiplicado=0;$multiplicado<=$multiplicador;$multiplicado++)
+{
+    $multiplicador=$multiplicado*$multiplicador;
+    echo $multiplicador;
+
+}
+
