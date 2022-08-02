6898fe5c198017a23d661e328dfc52dfa166a7fd
luciano <lucianogastonbarros@gmail.com>
2022-08-02 17:18:41 -0300

    correccion de errores archivo conexion.php

--- conexionphp/conexion.php
+++ conexionphp/conexion.php
@@ -1,14 +1,20 @@
 <?php
-conexionphp();
-function conexionphp(){
-    $server = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $dv = 'cerveceria';
-    $conectar = mysqli_connect($server, $user,$pass,$dv)or die (" Error en la conexion ");
-    return $conectar;
+/* $servername = "localhost";
+ $database = "cerveceria";
+ $username = "root";
+ $password = "";
+
+ $conectar = mysqli_connect($servername, $username, $password, $database,3306);*/
+ $servername = "localhost";
+ $database = "cerveceria";
+ $username = "root";
+ $password = "";
+ $port = " 3307 ";
+ //$conectar = mysqli_connect($servername, $username, $password, $database, $port);
+ $conectar = new mysqli($servername, $username, $password, $database, 3307);
+ if ($conectar->connect_errno) {
+    echo "Fallo al conectar a MySQL: (" . $conectar->connect_errno . ") " . $conectar->connect_error;
 }
 
+echo $conectar->host_info . "\n";
 
-
-?>
